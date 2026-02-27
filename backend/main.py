9c79e694717a45152a583c64db3b3802d49e76a7
Enjaneb <saeed.sqs.20@gmail.com>
2026-02-27 23:27:45 +0330

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,11 @@
-11
+import psutil
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/api/metrics")
+def get_metrics():
+    return {
+        "cpu": psutil.cpu_percent(interval=1),
+        "ram": psutil.virtual_memory().percent,
+        "status": "Online"
+    }
